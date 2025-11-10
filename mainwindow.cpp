fa3e00196b355d3da0162ecec8520d2b8c83805e
BrainB0ne <1330707+BrainB0ne@users.noreply.github.com>
2025-11-10 21:41:36 +0100

    convert password to NTLM hash

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QCryptographicHash>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +13,24 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_checkButton_clicked()
+{
+    QString strPassword = ui->passwordLineEdit->text();
+
+    QStringEncoder toUtf16 = QStringEncoder(QStringEncoder::Utf16LE);
+    QByteArray encodedUtf16String = toUtf16(strPassword);
+
+    QByteArray ba = QCryptographicHash::hash(encodedUtf16String, QCryptographicHash::Md4);
+
+    QString strHash = QString(ba.toHex().toUpper());
+
+    ui->hashLineEdit->setText(strHash);
+
+    ui->logPlainTextEdit->appendPlainText(
+                QString("Checking Password: %1 | Hash (NTLM): %2")
+                .arg(strPassword)
+                .arg(strHash));
 }
 
