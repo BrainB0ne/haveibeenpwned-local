b26527534e0a153f49c3bfa16f74ffc9fdb870e7
BrainB0ne <1330707+BrainB0ne@users.noreply.github.com>
2025-11-14 20:48:13 +0100

    added file.close()

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -184,6 +184,8 @@ void MainWindow::on_checkButton_clicked()
 
                             resTableDialog->deleteLater();
                         }
+
+                        file.close();
                     }
                 }
             }
