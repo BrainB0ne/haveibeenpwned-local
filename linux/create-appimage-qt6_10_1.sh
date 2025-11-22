450a13c8f6a60160f50a75eeb2261592ea19f205
BrainB0ne <1330707+BrainB0ne@users.noreply.github.com>
2025-11-22 19:26:51 +0100

    use qt from /usr/local/Qt-6.10.1 for linuxdeploy

--- linux/create-appimage-qt6_10_1.sh
+++ linux/create-appimage-qt6_10_1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # make sure to place linuxdeploy-plugin-qt-x86_64.AppImage in the same folder.
-export LD_LIBRARY_PATH=$HOME/Qt/6.10.1/gcc_64/lib:$LD_LIBRARY_PATH
-export QMAKE=$HOME/Qt/6.10.1/gcc_64/bin/qmake
+export LD_LIBRARY_PATH=/usr/local/Qt-6.10.1/lib:$LD_LIBRARY_PATH
+export QMAKE=/usr/local/Qt-6.10.1/bin/qmake
 
 ./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage --plugin qt
 
